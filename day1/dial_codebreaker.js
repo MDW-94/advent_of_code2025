09abf9013fcd039b778d79d0ab585732e762ba60
Matthew David Wasylko <matthew.wasylko@gmail.com>
2025-12-06 18:48:32 +0000

    refactor: MDW-0000: removes console.logs
    Signed-off-by: Matthew David Wasylko <matthew.wasylko@gmail.com>

--- day1/dial_codebreaker.js
+++ day1/dial_codebreaker.js
@@ -3,13 +3,9 @@ import fs from "fs";
 let textByLine = fs.readFileSync("code.txt").toString().split("\n");
 let zeroCounter = 0;
 
-// Dial goes from 0 ot 99
-// The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
 function twistDial(arrDialTwists) {
-  let dialVal = 50; // dial starts at 50???
+  let dialVal = 50; // dial starts at 50
   for (let i = 0; i < arrDialTwists.length; i++) {
-    // console.log(`${arrDialTwists[i][0]}`);
-
     switch (`${arrDialTwists[i][0]}`) {
       case "R":
         dialVal = (dialVal + parseInt(arrDialTwists[i].split("R")[1])) % 100;
@@ -32,10 +28,13 @@ function twistDial(arrDialTwists) {
   }
 }
 twistDial(textByLine);
-// console.log(textByLine);
 
 console.log(`Number of zeros: ${zeroCounter}`);
 
+// Dial goes from 0 ot 99
+// The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+
+// Resources:
 // Writing from console.log to file using fs: https://stackoverflow.com/questions/8393636/configure-node-js-to-log-to-a-file-instead-of-the-console
 
 // Continue: https://adventofcode.com/2025/day/1#part2
